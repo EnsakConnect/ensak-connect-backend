bff39880ad0ce5a01328577b533824feffb100bd
MouadBNL <mouadbnl.2k01@gmail.com>
2023-11-28 10:42:21 +0100

    chore: changed some test, and used env variables for the apllication.properties

--- src/test/java/com/ensak/connect/repository/TestRepositoryTest.java
+++ src/test/java/com/ensak/connect/repository/TestRepositoryTest.java
@@ -1,22 +1,23 @@
 package com.ensak.connect.repository;
 
-
-
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import com.ensak.connect.domain.TestEntity;
 
 import static org.junit.jupiter.api.Assertions.*;
+
 @DataJpaTest
 class TestRepositoryTest {
     @Autowired
     private TestRepository testRepository;
 
     @Test
-    public void saveTest_thenRetrieveTest() {
-        TestEntity savedTest = testRepository.save(new TestEntity(null, "Sample Test"));
+    public void itShouldSaveEntityToDatabase() {
+        String testName = "Sample Test";
+        TestEntity test = new TestEntity(testName);
+        TestEntity savedTest = testRepository.save(test);
         assertNotNull(savedTest.getId());
-        assertEquals("Sample Test", savedTest.getTest());
+        assertEquals(testName, savedTest.getTest());
     }
 }
