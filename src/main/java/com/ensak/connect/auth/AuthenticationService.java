39588b4dcf1382ce8864a140a63a3c77575f4cdf
MouadBNL <mouadbnl.2k01@gmail.com>
2023-12-02 21:12:26 +0100

    feat: fetch authenticated user from Authentication service

--- src/main/java/com/ensak/connect/auth/AuthenticationService.java
+++ src/main/java/com/ensak/connect/auth/AuthenticationService.java
@@ -6,6 +6,8 @@ import com.ensak.connect.user.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
@@ -46,5 +48,10 @@ public class AuthenticationService {
         return AuthenticationResponse.builder()
                 .token(jwtToken)
                 .build();
+    }
+
+    public User getAuthenticatedUser() {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        return (User) authentication.getPrincipal();
     }
 }
