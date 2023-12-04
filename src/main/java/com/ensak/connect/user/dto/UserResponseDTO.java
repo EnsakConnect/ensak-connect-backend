db027003bc99353fea96017b56b04e5e2ca94785
TheMadDrunk <iophamza2@gmail.com>
2023-12-04 02:17:35 +0100

    fix: updated UserResponseDTO

--- src/main/java/com/ensak/connect/user/dto/UserResponseDTO.java
+++ src/main/java/com/ensak/connect/user/dto/UserResponseDTO.java
@@ -10,16 +10,12 @@ import lombok.Data;
 @AllArgsConstructor
 public class UserResponseDTO {
     private Integer id;
-    private String firstname;
-    private String lastname;
     private String email;
     private String role;
 
     public static UserResponseDTO map(User user) {
         return UserResponseDTO.builder()
                 .id(user.getId())
-                .firstname(user.getFirstname())
-                .lastname(user.getLastname())
                 .email(user.getEmail())
                 .role(user.getRole().name())
                 .build();
