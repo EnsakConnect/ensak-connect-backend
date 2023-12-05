1155aa0e78660fc141ee2f465021879d745f3a4a
W5oAmI <ohassan864@gmail.com>
2023-12-05 15:52:39 +0100

    added model exception and exception handling

---
+++ src/main/java/com/ensak/connect/exception/model/EmailExistException.java
@@ -0,0 +1,7 @@
+package com.ensak.connect.exception.model;
+
+public class EmailExistException extends Exception {
+    public EmailExistException(String message) {
+        super(message);
+    }
+}
---
+++ src/main/java/com/ensak/connect/exception/model/EmailNotFoundException.java
@@ -0,0 +1,7 @@
+package com.ensak.connect.exception.model;
+
+public class EmailNotFoundException extends Exception {
+    public EmailNotFoundException(String message) {
+        super(message);
+    }
+}
