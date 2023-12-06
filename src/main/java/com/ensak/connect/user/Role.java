da20285712c4164d0d9f85277c4aaf15f948f8b7
W5oAmI <ohassan864@gmail.com>
2023-12-06 23:49:09 +0100

    modified role enum to handle permission

--- src/main/java/com/ensak/connect/user/Role.java
+++ src/main/java/com/ensak/connect/user/Role.java
@@ -1,13 +1,52 @@
 package com.ensak.connect.user;
 
 import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 
+import java.util.Collections;
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
 
-@Getter
+import static com.ensak.connect.user.Permission.*;
+
+@RequiredArgsConstructor
 public enum Role {
 
-    ROLE_USER,
-    ROLE_STUDENT,
-    ROLE_PROFESSOR,
-    ROLE_LAUREATE;
+  USER(Collections.emptySet()),
+  ADMIN(
+          Set.of(
+                  ADMIN_READ,
+                  ADMIN_UPDATE,
+                  ADMIN_DELETE,
+                  ADMIN_CREATE,
+                  MANAGER_READ,
+                  MANAGER_UPDATE,
+                  MANAGER_DELETE,
+                  MANAGER_CREATE
+          )
+  ),
+  MANAGER(
+          Set.of(
+                  MANAGER_READ,
+                  MANAGER_UPDATE,
+                  MANAGER_DELETE,
+                  MANAGER_CREATE
+          )
+  )
+
+  ;
+
+  @Getter
+  private final Set<Permission> permissions;
+
+  public List<SimpleGrantedAuthority> getAuthorities() {
+    var authorities = getPermissions()
+            .stream()
+            .map(permission -> new SimpleGrantedAuthority(permission.getPermission()))
+            .collect(Collectors.toList());
+    authorities.add(new SimpleGrantedAuthority("ROLE_" + this.name()));
+    return authorities;
+  }
 }
