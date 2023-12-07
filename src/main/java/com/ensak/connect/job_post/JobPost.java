9820a2022bb78dc3462647d76a459a79331a5e2e
W5oAmI <ohassan864@gmail.com>
2023-12-07 11:27:59 +0100

    updated job post model

--- src/main/java/com/ensak/connect/job_post/JobPost.java
+++ src/main/java/com/ensak/connect/job_post/JobPost.java
@@ -1,13 +1,19 @@
 package com.ensak.connect.job_post;
 
+import com.ensak.connect.comment_post.CommentPost;
+import com.ensak.connect.user.User;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
 import org.springframework.data.annotation.CreatedBy;
 
 import java.time.LocalDate;
+import java.util.Date;
+import java.util.List;
 
 @Entity
 @Data
@@ -22,8 +28,29 @@ public class JobPost {
 
     private String title;
 
-    private LocalDate createDate;
+    private String companyName;
+
+    private String location;
+
+    private String type;
 
     @Column(columnDefinition = "TEXT")
     private String description;
+
+    private List<String> tags;
+
+    private List<String> category;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id", nullable = false)
+    private User author;
+
+    @OneToMany(mappedBy = "questionPost", fetch = FetchType.LAZY, cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<CommentPost> comments;
+
+    @CreationTimestamp
+    private Date createdAt;
+
+    @UpdateTimestamp
+    private Date updatedAt;
 }
