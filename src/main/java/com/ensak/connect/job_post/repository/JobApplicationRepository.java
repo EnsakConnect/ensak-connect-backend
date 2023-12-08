ea1fe023fd6cd53d47a5bf5a08a3e5494c813197
TheMadDrunk <iophamza2@gmail.com>
2023-12-08 20:10:27 +0100

    feat: job application repository

--- src/main/java/com/ensak/connect/job_post/repository/JobApplicationRepository.java
+++ src/main/java/com/ensak/connect/job_post/repository/JobApplicationRepository.java
@@ -1,2 +1,12 @@
-package com.ensak.connect.job_post.repository;public interface JobApplicationRepository {
+package com.ensak.connect.job_post.repository;
+
+import com.ensak.connect.job_post.model.JobApplication;
+import com.ensak.connect.job_post.model.JobPost;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface JobApplicationRepository extends JpaRepository<JobApplication, Integer> {
+    public Optional<List<JobApplication>> findJobApplicationByJobPostId(Integer jobPostId);
 }
