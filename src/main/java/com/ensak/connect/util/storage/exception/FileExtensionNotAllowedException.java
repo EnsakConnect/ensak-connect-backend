ec6f87a4085fd2f7c830c37593bacb271cd87227
TheMadDrunk <iophamza2@gmail.com>
2023-12-08 18:51:49 +0100

    fix: gitignore bug

---
+++ src/main/java/com/ensak/connect/util/storage/exception/FileExtensionNotAllowedException.java
@@ -0,0 +1,5 @@
+package com.ensak.connect.util.storage.exception;
+
+public class FileExtensionNotAllowedException extends StorageException {
+    public FileExtensionNotAllowedException(String message) {super(message);}
+}
