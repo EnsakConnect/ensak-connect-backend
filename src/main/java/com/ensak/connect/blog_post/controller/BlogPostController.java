e2a321576ec8b0d40db8e74318a8fd570b7d7233
saad ohassan <ohassan864@gmail.com>
2024-01-05 13:21:52 +0100

    added blog post integration test

--- src/main/java/com/ensak/connect/blog_post/controller/BlogPostController.java
+++ src/main/java/com/ensak/connect/blog_post/controller/BlogPostController.java
@@ -18,7 +18,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/v1/job-posts")
+@RequestMapping("/api/v1/blog-posts")
 @RequiredArgsConstructor
 public class BlogPostController {
 
