5ecab7122c56735e35c2f229d4e1960a62d836fd
saad ohassan <ohassan864@gmail.com>
2024-01-05 12:37:44 +0100

    create blog post model

---
+++ src/main/java/com/ensak/connect/blog_post/model/BlogPost.java
@@ -0,0 +1,62 @@
+package com.ensak.connect.blog_post.model;
+
+import com.ensak.connect.auth.model.User;
+import com.ensak.connect.job_post.model.CommentPost;
+import com.ensak.connect.job_post.model.JobApplication;
+import com.ensak.connect.resource.model.Resource;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+@Entity
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class BlogPost {
+
+    @Id
+    @GeneratedValue
+    private Integer id;
+
+    @Column(columnDefinition = "TEXT")
+    private String content;
+
+    @ElementCollection
+    private List<String> tags = new ArrayList<>();
+
+    @ElementCollection
+    private List<Integer> likes = new ArrayList<>();
+
+    @ManyToOne
+    @JoinColumn(name = "user_id", nullable = false)
+    private User author;
+
+    @JsonIgnore
+    @OneToMany(mappedBy = "jobPost", fetch = FetchType.LAZY, cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<CommentPost> comments;
+
+
+    @OneToMany
+    @JoinTable(
+            name = "blog_post_resources",
+            joinColumns = @JoinColumn(name = "blog_post_id"),
+            inverseJoinColumns = @JoinColumn(name = "resource_id")
+    )
+    private List<Resource> resources;
+
+    @CreationTimestamp
+    private Date createdAt;
+
+    @UpdateTimestamp
+    private Date updatedAt;
+}
