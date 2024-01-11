603ad12cd537c20ee9bd5a431e3be588e0a94a4c
TheMadDrunk <iophamza2@gmail.com>
2024-01-11 17:16:19 +0100

    feat: Report DTO

--- src/main/java/com/ensak/connect/report/ReportRequestDTO.java
+++ src/main/java/com/ensak/connect/report/ReportRequestDTO.java
@@ -1,2 +1,28 @@
-package com.ensak.connect.report;public class ReportRequestDTO {
+package com.ensak.connect.report;
+
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class ReportRequestDTO {
+
+    @NotBlank(message = "postType is required")
+    @Pattern(regexp = "JOB|BLOG|QUESTION|ANSWER|COMMENT", message = "postType must be JOB, BLOG, QUESTION, ANSWER or COMMENT")
+    private String postType;
+
+    @NotNull(message = "postId should not be null")
+    @Min(value = 1, message = "postId can't be negative")
+    private Integer postId;
+
+    //optional
+    private String flag;
 }
