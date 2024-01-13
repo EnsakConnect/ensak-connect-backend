0c82b29d7bc500f6a886de1b27a856bb548964f4
Omar Defaoui <omardefaoui@gmail.com>
2024-01-13 18:35:36 +0100

    Get the last messages with the conversation infos

--- src/main/java/com/ensak/connect/chat_message/service/ChatMessageService.java
+++ src/main/java/com/ensak/connect/chat_message/service/ChatMessageService.java
@@ -3,7 +3,6 @@ package com.ensak.connect.chat_message.service;
 import com.ensak.connect.chat_message.dto.ChatMessageRequestDTO;
 import com.ensak.connect.chat_message.model.ChatMessage;
 import com.ensak.connect.chat_message.repository.ChatMessageRepository;
-import com.ensak.connect.config.exception.NotFoundException;
 import com.ensak.connect.conversation.model.Conversation;
 import com.ensak.connect.conversation.service.ConversationService;
 import lombok.RequiredArgsConstructor;
@@ -17,12 +16,6 @@ public class ChatMessageService {
 
     private final ChatMessageRepository chatMessageRepository;
     private final ConversationService conversationService;
-
-    public ChatMessage getChatMessageById(Integer id) {
-        return chatMessageRepository.findById(id).orElseThrow(
-                () -> new NotFoundException("Could not find chatMessage with id " + id + ".")
-        );
-    }
 
     // get only the chatMessages within one conversation
     public List<ChatMessage> getChatMessages(Integer conversationId) {
@@ -43,5 +36,18 @@ public class ChatMessageService {
                         .content(request.getContent())
                         .build()
         );
+    }
+
+    public ChatMessage getLastChatMessage(Integer conversationId) {
+        Conversation conversation = conversationService.getConversationById(conversationId);
+
+        List<ChatMessage> chatMessages = chatMessageRepository.findAll();
+        chatMessages.removeIf(chatMessage ->
+                !chatMessage.getConversationId().equals(conversation.getId()));
+
+        if (chatMessages.isEmpty()) {
+            return null;
+        }
+        return chatMessages.get(chatMessages.size() - 1);
     }
 }
